018e8d304a9542f9318bf6c23f10d43122baaced
s-hisham-gh <s.hisham2022@gmail.com>
2025-05-26 20:25:44 +0800

    Update p1.py

--- task1/p1.py
+++ task1/p1.py
@@ -1,1 +1,24 @@
-...
+import sys, grader, parse, collections
+
+def dfs_search(problem):
+    startState, goalState, stateSpaceGraph, heuristic = problem
+    frontier = collections.deque([[startState]])
+    elist = []
+    exploredSet = set()
+    
+    while frontier:  
+        path = frontier.pop() 
+        for i in goalState:
+            if (path[-1] == i):
+                return  ' '.join(elist)+'\n'+' '.join(path)         
+        if path[-1] not in exploredSet:
+            elist.append(path[-1])
+            exploredSet.add(path[-1])   
+            if path[-1] in stateSpaceGraph:
+                for child in stateSpaceGraph[path[-1]]: 
+                    frontier.append(path+[child[1]])
+
+if __name__ == "__main__":
+    test_case_id = int(sys.argv[1])
+    problem_id = 1
+    grader.grade(problem_id, test_case_id, dfs_search, parse.read_graph_search_problem)
